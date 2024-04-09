acb4d59d23768f85616cc97686dedaadb74978c3
RiyadM44 <riyad.murad@techvitis.com>
2024-04-09 14:08:59 +0300

    Database configuration

--- news_Application/news_Application/settings.py
+++ news_Application/news_Application/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'news_Application.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '3.78.242.227',
+        'PORT': '5432',
     }
 }
 
