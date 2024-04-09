083b9119999980c2a3a06ddf7de06b911b16b519
RiyadM44 <riyad.murad@techvitis.com>
2024-04-09 16:39:55 +0300

    png

--- news_Application/news_Application/settings.py
+++ news_Application/news_Application/settings.py
@@ -129,7 +129,12 @@ CSRF_TRUSTED_ORIGINS = ['https://newsaggregator.upskillconnect.com']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# settings.py
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
