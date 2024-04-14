e0587fb05b7207278622a0f339207a774cf57182
RiyadM44 <riyad.murad@techvitis.com>
2024-04-14 12:56:04 +0300

    Default carousel

--- news_Application/django_news/static/site.js
+++ news_Application/django_news/static/site.js
@@ -1,6 +1,28 @@
 let slideIndex = 0;
 showSlides();
 
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function moveSlide(n) {
+    slideIndex += n;
+    let slides = document.getElementsByClassName("carousel-slide");
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
 // function showSlides() {
 //     let slides = document.getElementsByClassName("carousel-slide");
 //     slideIndex++;
@@ -31,27 +53,3 @@ showSlides();
 //     // Move the current slide into view from the right
 //     slides[slideIndex-1].style.left = "0";
 // }
-
-function showSlides() {
-    let slides = document.getElementsByClassName("carousel-slide");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
-
-function moveSlide(n) {
-    slideIndex += n;
-    let slides = document.getElementsByClassName("carousel-slide");
-    if (slideIndex > slides.length) {slideIndex = 1}
-    if (slideIndex < 1) {slideIndex = slides.length}
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
-
-
