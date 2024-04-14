ce1e7ac3f185a3510d5d685632fcfb9d1d8211da
RiyadM44 <riyad.murad@techvitis.com>
2024-04-14 12:45:06 +0300

    normal

--- news_Application/django_news/static/site.js
+++ news_Application/django_news/static/site.js
@@ -1,45 +1,19 @@
 let slideIndex = 0;
 showSlides();
 
-function showSlides() {
-    let slides = document.getElementsByClassName("carousel-slide");
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    
-    // Slide all slides to the left
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.left = "-100%";
-    }
-    
-    // Move the current slide into view from the right
-    slides[slideIndex-1].style.left = "0";
-    
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
-
-function moveSlide(n) {
-    slideIndex += n;
-    let slides = document.getElementsByClassName("carousel-slide");
-    if (slideIndex > slides.length) {slideIndex = 1}
-    if (slideIndex < 1) {slideIndex = slides.length}
-    
-    // Slide all slides to the left
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.left = "-100%";
-    }
-    
-    // Move the current slide into view from the right
-    slides[slideIndex-1].style.left = "0";
-}
-
 // function showSlides() {
 //     let slides = document.getElementsByClassName("carousel-slide");
-//     for (let i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
 //     slideIndex++;
 //     if (slideIndex > slides.length) {slideIndex = 1}
-//     slides[slideIndex-1].style.display = "block";
+    
+//     // Slide all slides to the left
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.left = "-100%";
+//     }
+    
+//     // Move the current slide into view from the right
+//     slides[slideIndex-1].style.left = "0";
+    
 //     setTimeout(showSlides, 4000); // Change image every 4 seconds
 // }
 
@@ -48,10 +22,36 @@ function moveSlide(n) {
 //     let slides = document.getElementsByClassName("carousel-slide");
 //     if (slideIndex > slides.length) {slideIndex = 1}
 //     if (slideIndex < 1) {slideIndex = slides.length}
+    
+//     // Slide all slides to the left
 //     for (let i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
+//         slides[i].style.left = "-100%";
 //     }
-//     slides[slideIndex-1].style.display = "block";
+    
+//     // Move the current slide into view from the right
+//     slides[slideIndex-1].style.left = "0";
 // }
+
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function moveSlide(n) {
+    slideIndex += n;
+    let slides = document.getElementsByClassName("carousel-slide");
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
 
 
