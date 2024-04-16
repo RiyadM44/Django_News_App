d81df375514147ae9f482f4b79160bf79a1fad05
RiyadM44 <riyad.murad@techvitis.com>
2024-04-16 11:12:33 +0300

    test

--- news_Application/django_news/tests.py
+++ news_Application/django_news/tests.py
@@ -1,27 +1,27 @@
 # tests.py
 from django.test import TestCase
-from django.urls import reverse
-from django_news.models import NewsItem  # Corrected import statement
+# from django.urls import reverse
+# from django_news.models import NewsItem  # Corrected import statement
 
-class AddNewsFormTestCase(TestCase):
-    def test_add_news_form_submission(self):
-        form_data = {
-            'newsTitle': 'Test News Title',
-            'newsImageUrl': 'http://example.com/image.jpg',
-            'newsDescription': 'Test News Description',
-            'newsUrl': 'http://example.com/news',
-        }
-        # Count the number of news items before form submission
-        initial_news_count = NewsItem.objects.count()
+# class AddNewsFormTestCase(TestCase):
+#     def test_add_news_form_submission(self):
+#         form_data = {
+#             'newsTitle': 'Test News Title',
+#             'newsImageUrl': 'http://example.com/image.jpg',
+#             'newsDescription': 'Test News Description',
+#             'newsUrl': 'http://example.com/news',
+#         }
+#         # Count the number of news items before form submission
+#         initial_news_count = NewsItem.objects.count()
  
-        # Submit the form
-        self.client.post(reverse('save_text_input'), form_data)
+#         # Submit the form
+#         self.client.post(reverse('save_text_input'), form_data)
  
-        # Count the number of news items after form submission
-        final_news_count = NewsItem.objects.count()
+#         # Count the number of news items after form submission
+#         final_news_count = NewsItem.objects.count()
  
-        # Ensure that the news count increased by 1 after form submission
-        self.assertEqual(final_news_count, initial_news_count + 1, f"Expected {initial_news_count + 1} news items after form submission, but found {final_news_count}.")
+#         # Ensure that the news count increased by 1 after form submission
+#         self.assertEqual(final_news_count, initial_news_count + 1, f"Expected {initial_news_count + 1} news items after form submission, but found {final_news_count}.")
 
 # Create your tests here.
 
