ba8edf321a9efbcf729d19870b7ea61372725880
RiyadM44 <riyad.murad@techvitis.com>
2024-04-17 10:21:51 +0300

    Fixed API date

--- news_Application/django_news/views.py
+++ news_Application/django_news/views.py
@@ -10,8 +10,8 @@ def home(request):
     query = request.GET.get('query')  # Get the search query from the request
     api_key = 'fbc50f8083bb4dada1367e8345947f14'
     current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-    # url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-16&sortBy=publishedAt&apiKey={api_key}'
-    url = f'https://newsapi.org/v2/everything?q={query}&from={current_date}&sortBy=publishedAt&apiKey={api_key}'
+    url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-17&sortBy=publishedAt&apiKey={api_key}'
+    # url = f'https://newsapi.org/v2/everything?q={query}&from={current_date}&sortBy=publishedAt&apiKey={api_key}'
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
