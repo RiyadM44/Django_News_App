8626926e0044584673c8b223e0c67492b8608b67
RiyadM44 <riyad.murad@techvitis.com>
2024-04-17 10:30:42 +0300

    views

--- news_Application/django_news/views.py
+++ news_Application/django_news/views.py
@@ -1,7 +1,6 @@
 # views.py
 from django.shortcuts import render
 import requests
-import datetime
 
 def save_text_input(request):
     return render(request, 'index.html')
@@ -9,9 +8,7 @@ def save_text_input(request):
 def home(request):
     query = request.GET.get('query')  # Get the search query from the request
     api_key = 'fbc50f8083bb4dada1367e8345947f14'
-    current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-    url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-17&sortBy=publishedAt&apiKey={api_key}'
-    # url = f'https://newsapi.org/v2/everything?q={query}&from={current_date}&sortBy=publishedAt&apiKey={api_key}'
+    url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-17&sortBy=publishedAt&apiKey={api_key}'    
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
